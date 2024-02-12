b0ea6099d9a1fc8217aa6b187c1095056e754a76
nchi2 <rsnchi2@gmail.com>
2024-02-12 16:00:48 +0900

    Add Img - Products.js

--- components/Products.js
+++ components/Products.js
@@ -26,11 +26,11 @@ const StyledNowBookTitle = styled.div`
   border-bottom: 1px solid lightgray;
 `;
 const StyledNowBookList = styled.div`
-    width: 959px;
-    height: 368px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 959px;
+  height: 368px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const StyledNowBookListBox = styled.div`
   width: 885px;
@@ -84,7 +84,7 @@ const StyledProductBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border: 1px solid lightgray;  
+  border: 1px solid lightgray;
 `;
 const StyledProductImage = styled.div`
   height: 65%;
@@ -110,73 +110,73 @@ const StyledProductDescription = styled.div`
   flex-grow: 1;
 `;
 const StyledProductPrice = styled.div`
-    margin-bottom: 1.2rem;
-    padding: 0rem 1rem;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    font-size: 14px;
-    font-weight: bold;
+  margin-bottom: 1.2rem;
+  padding: 0rem 1rem;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 14px;
+  font-weight: bold;
 `;
 
 export default function Products() {
-    // 상품 데이터
-    const products = [
-        {
-            imageUrl: "상품1이미지URL",
-            name: "비트코인 필승 투자 스터디",
-            description: "오태민 작가와 함께하는 비트코인 투자 스터디",
-            price: "₩99,000",
-        },
-        {
-            imageUrl: "상품2이미지URL",
-            name: "비트모빅 로고 동판 - 몬트빌",
-            description: "2023년 7월 18일 몬트빌 에어드랍 기념 동판",
-            price: "₩5,000,000",
-        },
-        {
-            imageUrl: "상품3이미지URL",
-            name: "비트코인 투자 바이블",
-            description: "대한민국 비트코인 국내최고 권위자 오태민 작가",
-            price: "₩270,000",
-        },
-        {
-            imageUrl: "상품4이미지URL",
-            name: "모빌렛 - 5차",
-            description: "당신의 암호화폐를 안전하게 보관하세요.",
-            price: "₩40,000",
-        },
-    ];
+  // 상품 데이터
+  const products = [
+    {
+      imageUrl: "/images/BitcoinInvestmentBible.png",
+      name: "비트코인 필승 투자 스터디",
+      description: "오태민 작가와 함께하는 비트코인 투자 스터디",
+      price: "₩99,000",
+    },
+    {
+      imageUrl: "/images/Btcmobick_Logo_Goods2.png",
+      name: "비트모빅 로고 동판 - 몬트빌",
+      description: "2023년 7월 18일 몬트빌 에어드랍 기념 동판",
+      price: "₩5,000,000",
+    },
+    {
+      imageUrl: "/images/BitcoinInvestmentStudies .png",
+      name: "비트코인 투자 바이블",
+      description: "대한민국 비트코인 국내최고 권위자 오태민 작가",
+      price: "₩270,000",
+    },
+    {
+      imageUrl: "/images/Wallet_5th.png",
+      name: "모빌렛 - 5차",
+      description: "당신의 암호화폐를 안전하게 보관하세요.",
+      price: "₩40,000",
+    },
+  ];
 
-    return (
-        <StyledTopMidWraper>
-            <StyledNowBook>
-                <StyledNowBookTitle>카테고리별 판매량 1위</StyledNowBookTitle>
-                <StyledNowBookList>
-                    <StyledNowBookListBox>
-                        {products.map((product, index) => (
-                            <StyledProductBox key={index}>
-                                <StyledProductImage
-                                    style={{ backgroundImage: `url(${product.imageUrl})` }}
-                                />
-                                <StyledProductName>{product.name}</StyledProductName>
-                                <StyledProductDescription>
-                                    {product.description}
-                                </StyledProductDescription>
-                                <StyledProductPrice>{product.price}</StyledProductPrice>
-                            </StyledProductBox>
-                        ))}
-                    </StyledNowBookListBox>
-                </StyledNowBookList>
-            </StyledNowBook>
-            <StyledPubBook>
-                <StyledPubBookTitle>오독클 추천 서적</StyledPubBookTitle>
-                <StyledPubBookImageBox>
-                    <StyledPubBookImage>이미지</StyledPubBookImage>
-                    <StyledPubBookLine></StyledPubBookLine>
-                    <StyledPubBookImage>이미지</StyledPubBookImage>
-                </StyledPubBookImageBox>
-            </StyledPubBook>
-        </StyledTopMidWraper>
-    );
+  return (
+    <StyledTopMidWraper>
+      <StyledNowBook>
+        <StyledNowBookTitle>카테고리별 판매량 1위</StyledNowBookTitle>
+        <StyledNowBookList>
+          <StyledNowBookListBox>
+            {products.map((product, index) => (
+              <StyledProductBox key={index}>
+                <StyledProductImage>
+                  <img src={product.imageUrl} alt="4thWallet" />
+                </StyledProductImage>
+                <StyledProductName>{product.name}</StyledProductName>
+                <StyledProductDescription>
+                  {product.description}
+                </StyledProductDescription>
+                <StyledProductPrice>{product.price}</StyledProductPrice>
+              </StyledProductBox>
+            ))}
+          </StyledNowBookListBox>
+        </StyledNowBookList>
+      </StyledNowBook>
+      <StyledPubBook>
+        <StyledPubBookTitle>오독클 추천 서적</StyledPubBookTitle>
+        <StyledPubBookImageBox>
+          <StyledPubBookImage>이미지</StyledPubBookImage>
+          <StyledPubBookLine></StyledPubBookLine>
+          <StyledPubBookImage>이미지</StyledPubBookImage>
+        </StyledPubBookImageBox>
+      </StyledPubBook>
+    </StyledTopMidWraper>
+  );
 }
