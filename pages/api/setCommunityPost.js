e2e8f77498f145f1d40cd7f4a981a85d0d04aece
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2024-03-27 15:02:22 +0900

    fix api setCommunityPost.js

--- pages/api/setCommunityPost.js
+++ pages/api/setCommunityPost.js
@@ -6,18 +6,19 @@ export default async function handler(req, res) {
     try {
         console.log('req.body', req.body);
         const { title, nickname, password, content } = req.body;
-        if (req.method === 'POST') {
-            const data = await client.dbCommunityPost.create({
-                data: {
-                    title: title,
-                    nickname: nickname,
-                    password: password,
-                    content: content
-                }
-            });
-            console.log('data', data);
-            res.status(200).json(data);
-        }
+
+
+        const data = await client.dbCommunityPost.create({
+            data: {
+                title: title,
+                nickname: nickname,
+                password: password,
+                content: content
+            }
+        });
+        console.log('data', data);
+        res.status(200).json(data);
+
     } catch (e) {
         console.log(e);
         res.status(500).json({ message: 'Internal server error' });
